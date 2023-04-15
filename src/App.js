a94a9a3a99f2917b18c45b77ced7108716d095bc
mohan062002 <2020bcs037@sggs.ac.in>
2023-04-15 22:41:48 +0530

    twice commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <p className="text-xl font-mono text-red-600">Welcome to coding world</p>
+    </div>
+  );
+}
+
+export default App;
